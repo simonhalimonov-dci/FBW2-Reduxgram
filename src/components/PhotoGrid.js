8430f43e6e66a3aba1ee6f9a160788434feb48ae
Simon Halimonov Devugees-Laptop <simon.halimonov@devugees.org>
2018-08-12 15:06:00 +0200

    Display posts PhotoGrid
    Display post and comments in Single

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
 
 class PhotoGrid extends React.Component {
 
   render () {
+    console.log("PhotoGrid", this);
+
+    const postsData = this.props.posts
+    
+
     return (
       <div className="photo-grid">
         PhotoGrid
+
+        <div>
+          {postsData.map((post, i) => {
+            return (
+              <div key={i}>
+                <img src={post.display_src} alt=""/>
+              </div>
+            )
+          })}
+
+        </div>
       </div>
     )
   }
 }
 
-export default PhotoGrid;
+export default connect(mapStateToProps)(PhotoGrid);
--- src/components/Single.js
+++ src/components/Single.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
+
 class Single extends React.Component {
 
   render () {
     console.log("SINGLE POST", this);
+
+    // Filter the corresponding post via id that is passed through the router
+    const postData = this.props.posts.filter(post => post.id == this.props.match.params.id)[0]
+
+    console.log("THE POST DATA", postData);
+    
+    const postComments = this.props.comments[postData.code]
+
+    console.log("The Comment data for this post: ", postComments);
+    
     
 
     return (
       <div className="single-photo">
-        SINGLE: {this.props.match.params.id}
+        SINGLE: {postData.id}
+
+        <img src={postData.display_src} alt=""/>
+
+        <p>Caption: {postData.caption}</p>
+        <p>Likes: {postData.likes}</p>
+
+        <div>
+          {postComments.map((comment, i) => {
+            return (
+              <p key={i}>
+                {comment.user}: {comment.text} 
+              </p>
+            )
+          })}
+
+        </div>
       </div>
     )
   }
 }
 
-export default connect()(Single);
+export default connect(mapStateToProps)(Single);
